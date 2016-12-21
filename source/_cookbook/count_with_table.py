52a73e17e16dd9bc24156ee90a918bc480928fa1
Thomas A Caswell <tcaswell@bnl.gov>
2016-12-20 20:58:09 -0500

    MNT: tweak rst

--- source/_cookbook/count_with_table.py
+++ source/_cookbook/count_with_table.py
@@ -1,21 +1,22 @@
 """
-Simply 'count' a detector
-*************************
+===========================
+ Simply 'count' a detector
+===========================
 
 Problem
--------
+=======
 
 Take five sequential readings of a detector.
 
 Approach
---------
+========
 
 Execute :func:`bluesky.plans.count` with a simulated detector (which could be
 substituted by a real detector from ophyd) and display the reading using
 :func:`bluesky.callbacks.LiveTable`.
 
 Example Solution
-----------------
+================
 """
 import bluesky.plans as bp
 import bluesky.callbacks as bc
